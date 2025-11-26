f54a8286d598eb9972407da22c2f87e340e2dd72
williansales404 <willian9542@gmail.com>
2025-11-26 11:36:44 -0300

    inserir dados no jdbc com retorn das chaves

--- Banco_de_dados/JDBC_inserir_dados/src/application/Program.java
+++ Banco_de_dados/JDBC_inserir_dados/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -22,25 +23,35 @@ public class Program {
 			conn = DB.getConnection();
 			st = conn.prepareStatement(
 				"insert into seller" 
-				+ "(Id, Name, Email, BirthDate, BaseSalary, DepartmentId)"
-				+ "values" + "(?,?,?,?,?,?)");
+				+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+				+ "values" 
+				+ "(?,?,?,?,?)",
+				st.RETURN_GENERATED_KEYS);
 			
-			st.setInt(1, 5);
-			st.setString(2, "Carl Purple");
-			st.setString(3, "carl@gmail.com");
+			st.setString(1, "Sam black");
+			st.setString(2, "Sam@gmail.com");
 			// st.setDate(3, new java.sql.Date(fmt.parse("01/01/2020").getTime()));
 			// Parse usando java.time (API moderna)
-			LocalDate birthDate = LocalDate.parse("01/01/1998", fmt);
+			LocalDate birthDate = LocalDate.parse("01/01/1988", fmt);
 			// Opção 1 (compatível amplamente): converter para java.sql.Date
-			st.setDate(4, java.sql.Date.valueOf(birthDate));
+			st.setDate(3, java.sql.Date.valueOf(birthDate));
 			// Opção 2 (se o driver JDBC suporta JDBC 4.2+):
 			// st.setObject(3, birthDate);
-			st.setDouble(5, 3000.0);
-			st.setInt(6, 4);
+			st.setDouble(4, 3000.0);
+			st.setInt(5, 4);
 			
 			int affected_lines = st.executeUpdate(); // altera os dados
 			
-			System.out.println("linhas afetadas: " + affected_lines);
+			if(affected_lines > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("ID " + id);
+				}
+			}
+			else {
+				System.out.println("no lines affected");
+			}
 			
 		} catch (SQLException e) {
 			e.printStackTrace();

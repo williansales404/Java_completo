d202cb71c0069b93b9ee81ab15bc533ae03a2920
Williansales404 <willian9542@gmail.com>
2025-12-01 09:39:00 -0300

    atualização inserir dados usando textblock para dar os comandos sql

--- Banco_de_dados/JDBC_inserir_dados/src/application/Program.java
+++ Banco_de_dados/JDBC_inserir_dados/src/application/Program.java
@@ -22,49 +22,48 @@ public class Program {
 
 		try {
 			conn = DB.getConnection();
-			
-			/*	//Instancia objeto st
-			 * 	//Comando SQL
-			    st = conn.prepareStatement(
-				"insert into seller" 
-				+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-				+ "values" 
-				+ "(?,?,?,?,?)",
-				st.RETURN_GENERATED_KEYS);
-			//? espaço para ser preenchido depois com o metodo .set
-			st.setString(1, "Sam black");
+			/*
+			// Instancia objeto st
+			// Comando SQL
+			st = conn.prepareStatement("""
+					insert into seller
+					(Name, Email, BirthDate, BaseSalary, DepartmentId)
+					values (?,?,?,?,?)
+					""", st.RETURN_GENERATED_KEYS);
+			// ? espaço para ser preenchido depois com o metodo .set
+			st.setString(1, "Sam Black");
 			st.setString(2, "Sam@gmail.com");
-			
+
 			// st.setDate(3, new java.sql.Date(fmt.parse("01/01/2020").getTime()));
 			// Parse usando java.time (API moderna)
-			LocalDate birthDate = LocalDate.parse("01/01/1988", fmt);
+			LocalDate birthDate = LocalDate.parse("12/12/1999", fmt);
 			// Opção 1 (compatível amplamente): converter para java.sql.Date
 			st.setDate(3, java.sql.Date.valueOf(birthDate));
 			// Opção 2 (se o driver JDBC suporta JDBC 4.2+):
 			// st.setObject(3, birthDate);
-			
+
 			st.setDouble(4, 3000.0);
-			st.setInt(5, 4);*/
-			
+			st.setInt(5, 4);
+			*/
+//			
 			st = conn.prepareStatement(
 					"insert into department (Name)"
 					+ " values ('D1'), ('D2')",
 					Statement.RETURN_GENERATED_KEYS);
-			
+
 			int affected_lines = st.executeUpdate(); // altera os dados
-			
-			if(affected_lines > 0) {
-				//recuperar chaves geradas automaticamente pelo banco de dados
+
+			if (affected_lines > 0) {
+				// recuperar chaves geradas automaticamente pelo banco de dados
 				ResultSet rs = st.getGeneratedKeys();
-				while(rs.next()) {
+				while (rs.next()) {
 					int id = rs.getInt(1);
 					System.out.println("ID " + id);
 				}
-			}
-			else {
+			} else {
 				System.out.println("no lines affected");
 			}
-			
+
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
 		} finally {

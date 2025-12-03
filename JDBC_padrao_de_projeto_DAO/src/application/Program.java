cdb4f57db78df8d604819f3a3d3a0a201648a0dc
Williansales404 <willian9542@gmail.com>
2025-12-03 11:44:28 -0300

    criação classe seller no projeto DAO

--- JDBC_padrao_de_projeto_DAO/src/application/Program.java
+++ JDBC_padrao_de_projeto_DAO/src/application/Program.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +12,10 @@ public class Program {
 		
 		Department dp = new Department(1, "Black");
 		
+		Seller sl = new Seller(1, "Bob", "Bob@gamil.com", LocalDate.parse("01/01/2025", 
+				DateTimeFormatter.ofPattern("dd/MM/yyyy") ), 1000.0);
+		
+		System.out.println(sl);
 		System.out.println(dp);
 		
 	}

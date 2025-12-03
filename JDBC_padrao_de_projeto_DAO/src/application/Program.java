df7167257181561219f932aadddc007497d11aa1
Williansales404 <willian9542@gmail.com>
2025-12-03 11:10:10 -0300

    inicio da implementação de projeto padrão DAO

---
+++ JDBC_padrao_de_projeto_DAO/src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department dp = new Department(1, "Black");
+		
+		System.out.println(dp);
+		
+	}
+
+}

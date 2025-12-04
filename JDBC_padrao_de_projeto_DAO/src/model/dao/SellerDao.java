eacff85540e55eb681c998d87820ef1ec0259e21
Williansales404 <willian9542@gmail.com>
2025-12-04 09:50:11 -0300

    Criação das interfaces SellerDao e DepartmentDao no pacote model.entities

---
+++ JDBC_padrao_de_projeto_DAO/src/model/dao/SellerDao.java
@@ -0,0 +1,17 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Seller;
+
+public interface SellerDao {
+	void insert(Seller sl);
+
+	void update(Seller sl);
+
+	void deleteById(Integer id);
+
+	Seller findById(Integer id);
+
+	List<Seller> findAll();
+}

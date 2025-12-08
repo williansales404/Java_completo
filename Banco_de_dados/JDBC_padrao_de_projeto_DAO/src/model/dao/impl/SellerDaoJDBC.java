5c24ca6c0de8f421681922fa4356ce742d6168a7
Williansales404 <willian9542@gmail.com>
2025-12-08 11:40:21 -0300

    emplementação do metodo findByDepartment

--- Banco_de_dados/JDBC_padrao_de_projeto_DAO/src/model/dao/impl/SellerDaoJDBC.java
+++ Banco_de_dados/JDBC_padrao_de_projeto_DAO/src/model/dao/impl/SellerDaoJDBC.java
@@ -7,7 +7,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -88,7 +91,61 @@ public class SellerDaoJDBC implements SellerDao {
 		return null;
 	}
 
+	// Retorna uma lista de vendedores por departamento
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			// Comando SQL
+			st = conn.prepareStatement("""
+					SELECT seller.*,department.Name as DepName
+					FROM seller INNER JOIN department
+					ON seller.DepartmentId = department.Id
+					WHERE DepartmentId = ?
+					ORDER BY Name
+										""");
+			// Setando o parametro
+			st.setInt(1, department.getId());
+			// Executando o comando SQL
+			rs = st.executeQuery();
+			// Criando a lista de vendedores
+			List<Seller> list = new ArrayList<>();
+			// Mapa para evitar a criacao de multiplos departamentos
+			// Cada departamento sera instanciado uma unica vez
+			Map<Integer, Department> map = new HashMap<>();
+			
+			// Percorrendo o ResultSet e instanciando os vendedores
+			// Adicionando na lista de vendedores
+			while (rs.next()) {
+				// Verificando se o departamento ja foi instanciado
+				// Se ja foi instanciado, recupera do mapa
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				// Se nao foi instanciado, instancia e adiciona no mapa
+				if(dep == null) {
+					// Instanciando o Departamento	
+					dep = instanceDepartment(rs);
+					// Adicionando no mapa 
+					// Chave: Id do departamento
+					// Valor: Departamento instanciado
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				// Instanciando o Vendedor
+				Seller obj = instanceSeller(rs, dep);
+				list.add(obj);
+			}
+			// Retornando null caso nao encontre o vendedor
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+
 	// Método para instanciar um vendedor
+	// Recebe o ResultSet e o Departamento como parametros
 	private Seller instanceSeller(ResultSet rs, Department dp) throws SQLException {
 		Seller obj = new Seller();
 		obj.setId(rs.getInt("Id"));
@@ -101,6 +158,7 @@ public class SellerDaoJDBC implements SellerDao {
 	}
 
 	// Método para instanciar um departamento
+	// Recebe o ResultSet como parametro
 	private Department instanceDepartment(ResultSet rs) throws SQLException {
 		Department dp = new Department();
 		dp.setId(rs.getInt("DepartmentId"));

5e30925076ca03f4b844c4c05cf8ccf8a3055156
Williansales404 <willian9542@gmail.com>
2025-12-08 09:17:53 -0300

    metodos auxiliares para instanciar os objetos department e seller

--- Banco_de_dados/JDBC_padrao_de_projeto_DAO/src/application/Program.java
+++ Banco_de_dados/JDBC_padrao_de_projeto_DAO/src/application/Program.java
@@ -7,11 +7,11 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
+	
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("====== Test 1: seller findById ======");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 		
 	}
--- Banco_de_dados/JDBC_padrao_de_projeto_DAO/src/model/dao/impl/SellerDaoJDBC.java
+++ Banco_de_dados/JDBC_padrao_de_projeto_DAO/src/model/dao/impl/SellerDaoJDBC.java
@@ -15,12 +15,14 @@ import model.entities.Seller;
 //Implementação da interface SellerDao
 public class SellerDaoJDBC implements SellerDao {
 
-	//Atributo de conexao com o banco de dados
+	// Atributo de conexao com o banco de dados
+	// Conexao fechar somente quando a aplicacao for finalizada
+	// No projeto principal
 	private Connection conn;
 
-	//Construtor recebendo a conexao
+	// Construtor recebendo a conexao
 	public SellerDaoJDBC(Connection conn) {
-		//Inicializando o atributo conn
+		// Inicializando o atributo conn
 		this.conn = conn;
 	}
 
@@ -42,14 +44,14 @@ public class SellerDaoJDBC implements SellerDao {
 
 	}
 
-	//Retorna um vendedor pelo Id
+	// Retorna um vendedor pelo Id
 	@Override
 	public Seller findById(Integer id) {
 		PreparedStatement st = null;
 		ResultSet rs = null;
 
 		try {
-			//Comando SQL
+			// Comando SQL
 			st = conn.prepareStatement("""
 					select seller.*, department.Name as DepName
 					from seller
@@ -57,31 +59,19 @@ public class SellerDaoJDBC implements SellerDao {
 							on seller.DepartmentId = department.Id
 					where seller.Id = ?
 					""");
-			//Setando o parametro
+			// Setando o parametro
 			st.setInt(1, id);
-
-			//Executando o comando
+			// Executando o comando
 			rs = st.executeQuery();
-			//Verificando se retornou algum registro
+			// Verificando se retornou algum registro
 			if (rs.next()) {
-				//Instanciando o Departamento
-				Department dp = new Department();
-				dp.setId(rs.getInt("DepartmentId"));
-				dp.setName(rs.getString("DepName"));
-
-				//Instanciando o Vendedor
-				Seller obj = new Seller();
-				obj.setId(rs.getInt("Id"));
-				obj.setName(rs.getString("Name"));
-				obj.setEmail(rs.getString("Email"));
-				//
-				obj.setBirthDate(rs.getObject("BirthDate", LocalDate.class));
-				obj.setBaseSalary(rs.getDouble("BaseSalary"));
-				obj.setDepartment(dp);
+				// Instanciando o Departamento
+				Department dp = instanceDepartment(rs);
+				// Instanciando o Vendedor
+				Seller obj = instanceSeller(rs, dp);
 				return obj;
-
 			}
-			//Retornando null caso nao encontre o vendedor
+			// Retornando null caso nao encontre o vendedor
 			return null;
 
 		} catch (SQLException e) {
@@ -96,6 +86,26 @@ public class SellerDaoJDBC implements SellerDao {
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	// Método para instanciar um vendedor
+	private Seller instanceSeller(ResultSet rs, Department dp) throws SQLException {
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBirthDate(rs.getObject("BirthDate", LocalDate.class));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setDepartment(dp);
+		return obj;
+	}
+
+	// Método para instanciar um departamento
+	private Department instanceDepartment(ResultSet rs) throws SQLException {
+		Department dp = new Department();
+		dp.setId(rs.getInt("DepartmentId"));
+		dp.setName(rs.getString("DepName"));
+		return dp;
 	}
 
 }

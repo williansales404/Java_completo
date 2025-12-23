18e8da20a192cd2cffca8e3177d330b15ed0fce7
Williansales404 <willian9542@gmail.com>
2025-12-23 10:36:20 -0300

    Busca ao banco de dados com jpa

--- Spring_Boot/jpa_hipernate_nivelamento/src/main/java/application/Program.java
+++ Spring_Boot/jpa_hipernate_nivelamento/src/main/java/application/Program.java
@@ -10,19 +10,15 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
-		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
-		System.out.println("Pronto!");
+		Pessoa p = em.find(Pessoa.class, 2);
+		System.out.println(p);
+		
+		System.out.println("Pronto");
+		em.clear();
+		emf.close();
 	}
 
 }

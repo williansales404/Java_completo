a355a02b6ed5db98526e3b586e7ed89f34a55042
Williansales404 <willian9542@gmail.com>
2025-12-25 12:00:51 -0300

    criação dos arquivos e configuração H2 database, test profile, JPA

--- Spring_Boot/springboot_course/src/main/java/com/educandoweb/course/entities/User.java
+++ Spring_Boot/springboot_course/src/main/java/com/educandoweb/course/entities/User.java
@@ -2,10 +2,23 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
-	
+	// serialization usado para transformar o objeto em uma sequencia de bytes
+	// isso é necessario para trafegar na rede ou gravar em arquivos
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
--- Spring_Boot/springboot_course/src/main/java/com/educandoweb/course/resources/UserResource.java
+++ Spring_Boot/springboot_course/src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -13,7 +13,7 @@ public class UserResource {
 	
 	@GetMapping
 	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "Maria@gmail.com", "99999", "1234");
+		User u = new User(1L, "Maria", "maria@gmail.com", "99999", "1234");
 		return ResponseEntity.ok().body(u);
 	}
 }

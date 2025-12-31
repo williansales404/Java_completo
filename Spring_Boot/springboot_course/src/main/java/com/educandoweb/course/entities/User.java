2779e0fc91568405d2f94736cc1c7d5620a638eb
Williansales404 <willian9542@gmail.com>
2025-12-31 10:10:26 -0300

    Order, Instant, ISO 8601 part 1

--- Spring_Boot/springboot_course/src/main/java/com/educandoweb/course/entities/User.java
+++ Spring_Boot/springboot_course/src/main/java/com/educandoweb/course/entities/User.java
@@ -1,14 +1,15 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-
-
 
 @Entity
 @Table(name = "tb_user")
@@ -16,7 +17,7 @@ public class User implements Serializable {
 	// serialization usado para transformar o objeto em uma sequencia de bytes
 	// isso é necessario para trafegar na rede ou gravar em arquivos
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -24,6 +25,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 	}
@@ -75,6 +79,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override

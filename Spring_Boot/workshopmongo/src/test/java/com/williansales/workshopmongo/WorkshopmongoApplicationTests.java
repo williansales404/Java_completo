d6fe58fd5d92ae89a31a23793c0af61ddb6380e8
Williansales404 <willian9542@gmail.com>
2026-01-22 09:55:24 -0300

    projeto criado

---
+++ Spring_Boot/workshopmongo/src/test/java/com/williansales/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williansales.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

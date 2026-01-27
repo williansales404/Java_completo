06d5365de01d6658458719f096d9dcb804ed3b0d
Williansales404 <willian9542@gmail.com>
2026-01-27 09:12:31 -0300

    Usando padrão DTO para retornar usuários

---
+++ Spring_Boot/workshopmongo/src/main/java/com/williansales/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,49 @@
+package com.williansales.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.williansales.workshopmongo.domain.User;
+
+@JsonPropertyOrder({"id", "name", "email"}) // garante a orden dos atributos no json
+public class UserDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	private String email;
+
+	public UserDTO() {
+	}
+
+	public UserDTO(User obj) {
+		this.id = obj.getId();
+		this.name = obj.getName();
+		this.email = obj.getEmail();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
--- Spring_Boot/workshopmongo/src/main/java/com/williansales/workshopmongo/resources/UserResource.java
+++ Spring_Boot/workshopmongo/src/main/java/com/williansales/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.williansales.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.williansales.workshopmongo.domain.User;
+import com.williansales.workshopmongo.dto.UserDTO;
 import com.williansales.workshopmongo.services.UserService;
 
 @RestController
@@ -19,8 +21,9 @@ public class UserResource {
 	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }

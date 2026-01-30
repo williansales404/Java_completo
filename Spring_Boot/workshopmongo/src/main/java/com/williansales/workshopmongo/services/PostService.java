d6de6c03279562b79318aef54d2f224d16813335
Williansales404 <willian9542@gmail.com>
2026-01-30 10:24:01 -0300

    Obtendo um post por id

---
+++ Spring_Boot/workshopmongo/src/main/java/com/williansales/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.williansales.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.williansales.workshopmongo.domain.Post;
+import com.williansales.workshopmongo.repository.PostRepository;
+import com.williansales.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
